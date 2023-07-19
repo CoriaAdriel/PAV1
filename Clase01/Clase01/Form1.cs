fe7df2220d58de5cab0fc7b0861a20c7b2032fd6
Adriel Coria <coriadriel021@gmail.com>
2023-07-19 10:12:16 -0300

    Clase 01 Finalizada

---
+++ Clase01/Clase01/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clase01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LimpiarCampos();
+        }
+
+        private void btnMostrarDatos_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(txtNombre.Text + " " + txtApellido.Text);
+        }
+
+        private void btnAgregarPersona_Click(object sender, EventArgs e)
+        {
+            if(txtNombre.Text.Equals("") || txtApellido.Text.Equals(""))
+            {
+                MessageBox.Show("Complete nombre y apellido Por Favor....");
+            }
+            else
+            {
+                string nombreCompleto = "";
+                nombreCompleto = txtNombre.Text + " " + txtApellido.Text;
+                lstPersonas.Items.Add(nombreCompleto);
+                MessageBox.Show("Se ha agregado correctamente la persona!");
+                LimpiarCampos();
+            }
+        }
+        public void LimpiarCampos()
+        {
+            txtNombre.Text = "";
+            txtApellido.Text = "";
+        }
+
+        private void btnAgregarPersona2_Click(object sender, EventArgs e)
+        {
+            if (txtNombre.Text.Equals("") || txtApellido.Text.Equals(""))
+            {
+                MessageBox.Show("Complete nombre y apellido Por Favor....");
+            }
+            else
+            {
+                string nombreCompleto = "";
+                nombreCompleto = txtNombre.Text + " " + txtApellido.Text;
+                DialogResult resultado =  MessageBox.Show(nombreCompleto, "Alerta", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+                if(resultado == DialogResult.OK) 
+                {
+                    lstPersonas.Items.Add(nombreCompleto);
+                    MessageBox.Show("Se ha agregado correctamente la persona!");
+                    LimpiarCampos();
+                }
+                else
+                {
+                    MessageBox.Show("No se agrega la persona");
+                }
+            }
+        }
+    }
+}
